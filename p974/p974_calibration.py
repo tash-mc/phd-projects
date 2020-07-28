616b72ec88f39bf9838a8924260475a443e3fcec
Tash Marshall <natasha.mcfadden@students.mq.edu.au>
2020-07-28 15:54:22 +1000

    Bandpass calibration for scan included

--- p974/p974_calibration.py
+++ p974/p974_calibration.py
@@ -14,11 +14,60 @@ for r, d, f in os.walk(path):
         if '.hdf' in file:
             files.append(os.path.join(r, file))
 
-
-median_intensity = []
-
-
+# Section which should plot all the scans bandpass calibrated
 for file_name in files:
+
+    print(file_name.split("/")[1])
+
+    f = h5py.File(file_name, 'r')
+
+    f.keys()
+
+    freq = np.array(f.get('beam_0/band_SB5/astronomy_data/frequency'))
+    intensity = np.array(f.get('beam_0/band_SB5/astronomy_data/data'))
+
+    # Data array shape is given by [spectral dump][][polarisations][channels][]
+
+    chan_len = len(intensity[0][0][0])
+
+    # Create an off source spectra by using the mean of the spectra dumps between 3 and 10.
+
+    temp1 = []
+    temp2 = []
+    for i in range(2,10):
+        temp1.append(np.reshape(intensity[i][0][0], chan_len))
+        temp2.append(np.reshape(intensity[i][0][1], chan_len))
+
+    temp1 = np.array(temp1)
+    temp2 = np.array(temp2)
+
+    off_source_pol1 = np.mean(temp1, axis=0)
+    off_source_pol2 = np.mean(temp2, axis=0)
+
+    i_off = (off_source_pol1 + off_source_pol2) / 2
+
+    p_onoff = []
+    median_intensity = []
+
+    # Cycle through all spectral dumps
+    for dump in range(0,len(intensity)):
+
+        on_source_pol1 = np.reshape(intensity[dump][0][0], chan_len)
+        on_source_pol2 = np.reshape(intensity[dump][0][1], chan_len)
+
+        i_on = (on_source_pol1 + on_source_pol2) / 2
+
+        p_onoff.append((i_on - i_off) / i_off)
+
+    plt.axes(ylabel="Signal Strength [Counts]", xlabel="Spectral Dump Number")
+    plt.title("Bandpass Calibrated Scan")
+    plt.plot(range(0,len(intensity)), np.mean(p_onoff, axis= 1))
+
+plt.savefig("Bandpass Calibrated Scan.png")
+plt.show()
+
+# Section that will run the bandpass cal for the "on source" scan portion of 1934
+"""for file_name in files:
 
     f = h5py.File(file_name, 'r')
 
@@ -81,52 +130,3 @@ for file_name in files:
     plt.show()
 
     """
-    plt.axes(ylabel="Signal Strength [Counts]", xlabel="1934 Observation Files")
-    plt.plot(range(0, len(files)), median_intensity, linewidth=0.5)
-    plt.title("Plot of Average Signal Strength over Observations of 1934")
-    plt.savefig("Time Average.png")
-
-    
-    src_on_pol2 = np.reshape(intensity[56][0][1], 131072)
-
-    src_off_pol1 = np.reshape(intensity[3][0][0], 131072)
-    src_off_pol2 = np.reshape(intensity[3][0][1], 131072)
-
-
-    plt.plot(freq,src_on_pol2)
-    plt.plot(freq,src_on_pol1)
-    plt.plot(freq,src_off_pol1)
-    plt.plot(freq,src_off_pol2)
-    plt.savefig("1934_DEC.png")
-
-
-
-    i_on = (src_on_pol1 + src_on_pol2)/2
-    i_off = (src_off_pol1 + src_off_pol2)/2
-
-    p_onoff = ((i_on - i_off)/i_off)
-
-    index = np.where(freq == 1380.0002)
-
-    S_sys = 14.69/p_onoff[index]
-
-    print(S_sys)
-
-    p_onoff = S_sys*((i_on - i_off)/i_off)
-
-    fit = np.polyfit(freq,p_onoff,1)
-
-    array_1934 = [[1380,1413],[14.96, 14.87]]
-
-    fit_1934 = np.polyfit(array_1934[0],array_1934[1],1)
-
-    print(p_onoff.shape)
-
-    plt.axes(ylabel = "Signal Strength [Jy]", xlabel = "Frequency [MHz]")
-    plt.plot(freq,p_onoff, linewidth = 0.5)
-    plt.plot(freq,fit[0]*freq+fit[1], label = "Data Best Fit")
-    plt.plot(freq,fit_1934[0]*freq+fit_1934[1], label = "1934")
-    plt.title(file_name+ " Ssys value of: " + str(S_sys))
-    plt.legend()
-    plt.savefig("1934_DEC.png")
-"""
